e8fffb2ff35b75090bd9c8adb049e7b4982132af
Ratau Micek <32156532+rataumicek@users.noreply.github.com>
2022-03-05 23:12:26 +0700

    Update FeaturesSplit.js

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -54,8 +54,8 @@ class FeaturesSplit extends React.Component {
     );
 
     const sectionHeader = {
-      title: 'Workflow that just works',
-      paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+      title: 'From Community for Community',
+      paragraph: 'Noise Everyday is open source, we build a community that shares the same vision and mission for the future. Not only because of money, because money can come if you have creativity.'
     };
 
     return (
@@ -71,13 +71,13 @@ class FeaturesSplit extends React.Component {
               <div className="split-item">
                 <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                   <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                    Lightning fast workflow
+                    Artist
                   </div>
                   <h3 className="mt-0 mb-12">
-                    Data-driven insights
+                    Upgrade your World
                   </h3>
                   <p className="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    If you are an artist, now is the time to expand the reach of your audience through NFT because the world will be easier to recognize you.
                   </p>
                 </div>
                 <div className={
@@ -97,13 +97,13 @@ class FeaturesSplit extends React.Component {
               <div className="split-item">
                 <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                   <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                    Lightning fast workflow
+                    Blogger
                   </div>                  
                   <h3 className="mt-0 mb-12">
-                    Data-driven insights
+                    Expand Your Knowledge
                   </h3>
                   <p className="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Being a blogger is fun because you can share knowledge while learning. In addition, you can also earn income from ad impressions. In the future you will no longer need to serve ads on your personal blog, but make money directly into your crypto wallet that comes in from visitors to your blog.
                   </p>
                 </div>
                 <div className={
@@ -123,13 +123,13 @@ class FeaturesSplit extends React.Component {
               <div className="split-item">
                 <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                   <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                    Lightning fast workflow
+                    Web and App Developer
                   </div>                  
                   <h3 className="mt-0 mb-12">
-                    Data-driven insights
+                    Join to Web3
                   </h3>
                   <p className="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    It's no longer fun with web 2.0 and now is the time for you to switch to web 3.0 which is of course simpler and can generate many financial benefits more easily.
                   </p>
                 </div>
                 <div className={
