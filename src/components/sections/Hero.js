9bbd98cb9b6159ce52fc580904c392324321d4f5
Ratau Micek <32156532+rataumicek@users.noreply.github.com>
2022-03-05 22:45:02 +0700

    Update Hero.js

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -65,18 +65,18 @@ class HeroFull extends React.Component {
           <div className={innerClasses}>
             <div className="hero-content">
               <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-                Landing template for <span className="text-color-primary">startups</span>
+                JOIN THE <span className="text-color-primary">CRYPTO</span> OG CLUB
               </h1>
               <div className="container-xs">
                 <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                  Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                  Noise Everyday is an extraordinary spirit that we consciously build to be able to coexist with the future World.
                 </p>
                 <div className="reveal-from-bottom" data-reveal-delay="600">
                   <ButtonGroup>
-                    <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                    <Button tag="a" color="primary" wideMobile href="#about-us">
                       Get started
                     </Button>
-                    <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                    <Button tag="a" color="dark" wideMobile href="https://github.com/noiseeveryday">
                       View on Github
                     </Button>                    
                   </ButtonGroup>
